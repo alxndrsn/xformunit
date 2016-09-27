0670982700ff4248cda134550e8554a35a7228c5
Alex Anderson <anderson@medicmobile.org>
2016-09-27 15:35:24 +0200

    Include jquery in loaded page

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    browserify: {
+      www: {
+        src: ['src/www/index.js'],
+        dest: 'build/www/index.js',
+      },
+    },
+
     connect: {
       options: {
         port: 8888,
@@ -24,7 +31,15 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-protractor-runner');
+
+
+  grunt.registerTask('test', [
+    'browserify',
+    'connect',
+    'protractor',
+  ]);
 
 };
