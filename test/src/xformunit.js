8c10bd36ad63659a21f6cbca94c7ef26fbbb2933
Alex Anderson <anderson@medicmobile.org>
2016-09-27 13:44:13 +0200

    Flesh out basic loading tests

--- test/src/xformunit.js
+++ test/src/xformunit.js
@@ -2,8 +2,30 @@ var xformunit = require('../../src/js/xformunit');
 
 describe('xformunit test framework', function() {
   it('should throw an error when the requested form is not found', function() {
+    return xformunit.loadForm('../res/missing.xml')
+      .then(function() {
+        throw new Error('Loading a non-existent form should fail.');
+      })
+      .catch(function(err) {
+        // expected
+        // TODO we actually expect an error here, but until we can specify more
+        // details that we expect, it's more useful to throw it and fail the
+        // test.
+        throw err;
+      });
   });
   it('should throw an error when the requested form is poorly-formed', function() {
+    return xformunit.loadForm('../res/badly-formed.xml')
+      .then(function() {
+        throw new Error('Loading a file containing badly-formed XML should fail.');
+      })
+      .catch(function(err) {
+        // expected
+        // TODO we actually expect an error here, but until we can specify more
+        // details that we expect, it's more useful to throw it and fail the
+        // test.
+        throw err;
+      });
   });
   it('should return happily when a form loads successfully', function() {
     return xformunit.loadForm('../res/simple.xml');
